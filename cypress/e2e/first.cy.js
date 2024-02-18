6ce43b43e55f5f299d61a4680bbe07b796f09e23
Mark <markyanovskiy@gmail.com>
2024-02-18 15:06:21 +0100

    test fix

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,5 +1,5 @@
 describe(`First test suit`, ()=>{
     it(`First test`, ()=>{
-        CSSSkewY.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc");
+        CY.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc");
     })
 })
