d38b4defb6da01207bf4c51d5e2de1800b92a16e
Mark <markyanovskiy@gmail.com>
2024-02-19 01:17:13 +0100

    changes in cyperss config js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+    projectId: '6ce43b4',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,5 +1,5 @@
 describe(`First test suit`, ()=>{
     it(`First test`, ()=>{
-        CY.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc");
+        cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc");
     })
 })
