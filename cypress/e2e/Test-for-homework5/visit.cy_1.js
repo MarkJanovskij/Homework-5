cfc8606fe1740cc0d145560f3c42c521e6b6fb65
Mark <markyanovskiy@gmail.com>
2024-03-01 22:56:15 +0100

    1

--- cypress/e2e/Test-for-homework5/visit.cy_1.js
+++ cypress/e2e/Test-for-homework5/visit.cy_1.js
@@ -4,7 +4,7 @@ describe(`First test suit`, ()=>{
     })
 })
 
-it.only(`Test button account`,()=>{
+it(`Test button account`,()=>{
     cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc");
     cy.get(`[data-cy="accountMenu"]`).click( )
 })
