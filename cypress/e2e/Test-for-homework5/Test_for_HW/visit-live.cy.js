ddecfe6d69468986f6431c084e77c339ca0052e9
Mark <markyanovskiy@gmail.com>
2024-03-02 14:29:24 +0100

    change live

--- cypress/e2e/Test-for-homework5/Test_for_HW/visit-live.cy.js
+++ cypress/e2e/Test-for-homework5/Test_for_HW/visit-live.cy.js
@@ -4,46 +4,47 @@ describe("tests for sqlverifier-live", ()=>{
     })
 })
 
-it("Test button account",()=>{
-    cy.get(`[data-cy="accountMenu"]`).click( )
-})
+it("Test button Account", () => {
+    cy.get('[data-cy="accountMenu"] > .d-flex > span').click();
+});
 
-it("Test button Language English",()=>{
-    cy.get(`[data-cy="accountMenu"]`).click( )
-})
+it("Test button Language English", () => {
+    cy.get(':nth-child(2) > .d-flex > span').click();
+});
 
-it("Test button Home",()=>{
-    cy.get(".d-flex > :nth-child(2) > span").click( )
-})
+it("Test button Home", () => {
+    cy.get('.d-flex > :nth-child(2) > span').click();
+});
+
 
 it("Test button Brand",()=>{
-    cy.get(".brand-title > span").click( )
+    cy.get('.brand-title > span').click();
 })
 
 it("Test button Logo",()=>{
-    cy.get("img").click( )
+    cy.get('img').click();
 })
 
 it("Test button version",()=>{
-    cy.get(".navbar-version").click( )
+    cy.get('.navbar-version').click();
 })
 
 it("Test button 1 page",()=>{
-    cy.get(".active > .page-link").click( )
+    cy.get('.active > .page-link').click();
 })
 
 it("Test button 2 page",()=>{
-    cy.get(":nth-child(4) > .page-link").click( )
+    cy.get(':nth-child(4) > .page-link').click();
 })
 
 it("Test button 3 Page",()=>{
-    cy.get(":nth-child(5) > .page-link").click( )
+    cy.get(':nth-child(5) > .page-link').click();
 })
 
 it("Test button fast forward",()=>{
-    cy.get(":nth-child(8) > .page-link").click( )
+    cy.get(':nth-child(8) > .page-link').click();
 })
 
 it("Test button last page",()=>{
-    cy.get(":nth-child(9) > .page-link").click( )
+    cy.get(':nth-child(9) > .page-link').click();
 })
