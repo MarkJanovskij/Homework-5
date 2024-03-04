af96631395fbfc389c13652ffc078e316795ae80
Mark <markyanovskiy@gmail.com>
2024-03-04 15:31:23 +0100

    save before paralization

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   
-    projectId: 'byvzu8',
+    projectId: 'gf7az6',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/Test-for-homework5/Test_for_HW/visit-stading.cy.js
+++ cypress/e2e/Test-for-homework5/Test_for_HW/visit-stading.cy.js
@@ -3,6 +3,49 @@ describe('Test button Logo', () => {
       cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
   
       
+  
+      
+      cy.get('[data-cy="accountMenu"]').should('be.visible');
+  
+      
+      cy.get('[data-cy="accountMenu"]').click();
+
+    });
+  });
+
+
+  describe('Test button Language English', () => {
+    it('Test button Language English', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+      
+      cy.get(':nth-child(2) > .d-flex > span').should('be.visible').click();
+    });
+  });
+
+  describe('Test button Home', () => {
+    it('Test button Home', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+     
+      cy.get('.d-flex > :nth-child(2) > span').should('be.visible').click();
+    });
+  });
+
+  describe('Test button Brand', () => {
+    it('Test button Brand', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+      
+      cy.get('.brand-title > span').should('be.visible').click();
+    });
+  });
+
+  describe('Test button Logo', () => {
+    it('Test button Logo', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+      
       cy.get('img').should('be.visible').click();
     });
   });
@@ -22,5 +65,41 @@ describe('Test button Logo', () => {
   
       
       cy.get('.active > .page-link').should('be.visible').click();
+    });
+  });
+
+  describe('Test button 2 page', () => {
+    it('Test button 2 page', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+     
+      cy.get(':nth-child(4) > .page-link').should('be.visible').click();
+    });
+  });
+
+  describe('Test button 3 Page', () => {
+    it('Test button 3 Page', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+     
+      cy.get(':nth-child(5) > .page-link').should('be.visible').click();
+    });
+  });
+
+  describe('Test button fast forward', () => {
+    it('Test button fast forward', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+     
+      cy.get(':nth-child(8) > .page-link').should('be.visible').click();
+    });
+  });
+
+  describe('YTest button last page', () => {
+    it('Test button last page', () => {
+      cy.visit('https://sqlverifier-staging-08050d656f7a.herokuapp.com/');
+  
+      
+      cy.get(':nth-child(9) > .page-link').should('be.visible').click();
     });
   });
