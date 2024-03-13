cca8d41aa194685dad258eec924c20b7b885ddaf
Mark <markyanovskiy@gmail.com>
2024-03-13 19:32:49 +0100

    new cy confid did not work changed to old one

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
+const { defineConfig } = require("cypress");
+
 module.exports = defineConfig({
   projectId: "gf7az6",
   e2e: {
     baseUrl: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/",
+    specPattern: "cypress/e2e/Test-for-homework5/Test_for_HW/*.spec.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
